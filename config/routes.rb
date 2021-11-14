dcb27499e61b862586b0a6b6b5b52ba35799f71b
Max Fry <maxcfry@outlook.com>
2021-11-14 13:06:44 +0000

    Users can comment on posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root "articles#index"
-
-  resources :articles
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    root "articles#index"
+  
+    resources :articles do
+      resources :comments
+    end
+  end
+  
 end
 
