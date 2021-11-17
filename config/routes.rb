273933b674e14305577b99a2d53fd8db01dc33cc
Max Fry <maxcfry@outlook.com>
2021-11-17 12:44:55 +0000

    Home and about pages created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   Rails.application.routes.draw do
-    root "articles#index"
+
+    get "about-us", to: "about#index", as: :about
+
+    root to: "main#index"
+    get "sign_up", to: "registrations#new"
+    post "sign_up", to: "registrations#create"
+    delete "logout", to: "sessions#destroy"
+
+    # root to: "articles#index"
   
     resources :articles do
       resources :comments
